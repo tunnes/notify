310114138d06ba835ad52017ed1bc0c8031b9f99
Ayrton Felipe <ayrton.tunes@outlook.com>
2016-11-18 11:35:00 +0000

    Configuração do Boostrap no Yesod.

--- Foundation.hs
+++ Foundation.hs
@@ -2,12 +2,32 @@
 {-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeFamilies      #-}
 {-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE QuasiQuotes       #-} -- Importante quando se usa QuasiQuoters
+
 module Foundation where
 
-import Yesod.Core
+import Yesod
 
 data App = App
 
 mkYesodData "App" $(parseRoutesFile "routes")
 
-instance Yesod App
+instance Yesod App where
+-- Inserindo componentes para funcionamento do Boostrap:
+-- "Sobrescrevendo" a função defaultLayout. widgetToPageContent retorna uma estrutura separada em 'head' e 'body'.
+
+    defaultLayout widget = widgetToPageContent widget >>= \pageContent -> 
+        withUrlRenderer [hamlet|
+            <!doctype html>
+                <html lang="PT-BR">
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+                        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
+                        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js">
+                        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+                        ^{pageHead pageContent}
+                    ^{pageBody pageContent}
+        |]
+        
+    
