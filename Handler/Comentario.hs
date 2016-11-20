3a09d1859e8143c21da8bf3a55115e356eec5b39
Tarcisio Lima <tarcisio.bomdebola@bol.com.br>
2016-11-20 21:07:44 +0000

    Implementação do formulario do jornalista, login/cadastro

--- Handler/Comentario.hs
+++ Handler/Comentario.hs
@@ -6,6 +6,14 @@ module Handler.Comentario where
 
 import Foundation
 import Yesod.Core
+import Control.Applicative
+import Yesod.Form.Bootstrap3
+import Database.Persist.Postgresql
+
+{--comentarioForm :: Form Comentario
+comentarioForm = renderDivs $ Comentario
+     <$> areq emailField (bfs ("E-mail" :: Text)) Nothing 
+     <*> areq textareaField (bfs ("Comentário" :: Text)) Nothing --}
 
 -- Recebe um post com o comentario e o ID da noticia no qual receberá o comentário
 postComentarioR :: NoticiaId -> Handler Html
